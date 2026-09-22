be47d248ebeb3260ae4f980767d89553c3407477
Santimartdev <Santimart222@gmail.com>
2026-09-22 17:13:52 -0600

    Mejoré cosas del movimiento y agregué variables

--- Assets/SantiagoM/Códigomart/PlayerController.cs
+++ Assets/SantiagoM/Códigomart/PlayerController.cs
@@ -1,4 +1,6 @@
 using JetBrains.Annotations;
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -6,14 +8,16 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Estadísticas del player")]
     public int PlayerHealth = 6;
-    private float playerSpeed = 1.0f;
-    public float MovementSpeed = 1.0f;
+    private float playerSpeed = 5.0f;
+    public float SpeedStat = 1.0f;
     public float PlayerPoison = 0f;
-    public float PlayerDamage = 4f;
+    public float PlayerDamage = 3f;
     public float AttackRange = 1.5f;
     public float AttackCooldown = 2f;
     public float PlayerLuck = 1f;
+    private Vector2 playerMoves;
     private Rigidbody2D _playerRigidBody2D;
+    public List<string> Inventory;
     void Start()
     {
        _playerRigidBody2D = GetComponent<Rigidbody2D>();
@@ -24,11 +28,21 @@ public class PlayerController : MonoBehaviour
         PlayerMovement();
     }
 
+    private void FixedUpdate()
+    {
+        
+    }
+
     public void PlayerMovement()
     {
-        float ActualPlayerSpeed = playerSpeed * MovementSpeed; 
+        float ActualPlayerSpeed = playerSpeed * SpeedStat; 
         float HorizontalMovement = Input.GetAxisRaw("Horizontal");
         float VerticalMovement = Input.GetAxisRaw("Vertical");
-        _playerRigidBody2D.linearVelocity = new Vector2(HorizontalMovement * ActualPlayerSpeed, VerticalMovement * ActualPlayerSpeed).normalized;
+        playerMoves = new Vector2(HorizontalMovement, VerticalMovement).normalized;
+        _playerRigidBody2D.linearVelocity = new Vector2(HorizontalMovement * ActualPlayerSpeed, VerticalMovement * ActualPlayerSpeed);
+    }
+    public void Attack()
+    {
+
     }
 }
